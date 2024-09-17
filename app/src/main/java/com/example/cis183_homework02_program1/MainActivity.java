74a81175bd4a7bacbb3781911e5e39dc9006fcbd
TMcBride-MCCC <tmcbride2@my.monroeccc.edu>
2024-09-16 21:36:31 -0400

    GUI
    -Created GUI elements
    -Tied GUI elements to Java variables
    -Added comments for needed functions
    
    Next up:
    1. Create Color class
    2. Create ArrayList<Color>

--- app/src/main/java/com/example/cis183_homework02_program1/MainActivity.java
+++ app/src/main/java/com/example/cis183_homework02_program1/MainActivity.java
@@ -1,19 +1,33 @@
 package com.example.cis183_homework02_program1;
 
-import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity
 {
-    //Create Java variables [HERE]
-    TextView tv_j_title;
+    //Creating Java variables for GUI
+    TextView tv_j_am_redNumVal;
+    SeekBar sb_j_am_redSlider;
+
+    TextView tv_j_am_greenNumVal;
+    SeekBar sb_j_am_greenSlider;
+
+    TextView tv_j_am_blueNumVal;
+    SeekBar sb_j_am_blueSlider;
+
+    TextView tv_j_am_hexVal;
+
+    Button btn_j_am_save;
+
+    ListView lv_j_am_listOfColors;
+
+    //ArrayList class here
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,10 +37,38 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         //Connect the Java variables to corresponding GUI variables [HERE]
-        tv_j_title = findViewById(R.id.tv_v_am_title);
+        tv_j_am_redNumVal = findViewById(R.id.tv_v_am_redNumVal);
+        sb_j_am_redSlider = findViewById(R.id.sb_v_am_redSlider);
 
-        //Set Title text & background color
-        //tv_j_title.setTextColor(Color.BLACK);
-        //tv_j_title.setBackgroundColor(Color.CYAN);
+        tv_j_am_greenNumVal = findViewById(R.id.tv_v_am_greenNumVal);
+        sb_j_am_greenSlider = findViewById(R.id.sb_v_am_greenSlider);
+
+        tv_j_am_blueNumVal = findViewById(R.id.tv_v_am_blueNumVal);
+        sb_j_am_blueSlider = findViewById(R.id.sb_v_am_blueSlider);
+
+        tv_j_am_hexVal = findViewById(R.id.tv_v_am_hexVal);
+
+        btn_j_am_save = findViewById(R.id.btn_v_am_save);
+
+        lv_j_am_listOfColors = findViewById(R.id.lv_v_am_listOfColors);
+
+        //Event listener function for red slider
+
+        //Event listener function for green slider
+
+        //Event listener function for blue slider
+
+        //Set on click listener function for save button
+
+        //new ArrayList
+
+        //Function to fill list
+
     }
+
+    //Function for button click here
+
+
+
+
 }
