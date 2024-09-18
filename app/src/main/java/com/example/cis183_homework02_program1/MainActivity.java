95c1d7c3de00da1d02477fb75b2326c814ee900b
TMcBride-MCCC <tmcbride2@my.monroeccc.edu>
2024-09-17 21:22:07 -0400

    What a day.....
    Ran into a problem with the sliders....deleted all code and started over....
    
    -Sliders are working properly!
    -NumVals pull the slider value properly!
    -Hex code is *kind of* working....
    
    Next:
    -Add if statements to sb listener functions to add a zero in the event that the int pulled is 1 digit.

--- app/src/main/java/com/example/cis183_homework02_program1/MainActivity.java
+++ app/src/main/java/com/example/cis183_homework02_program1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.cis183_homework02_program1;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -15,23 +17,17 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity
 {
     //Creating Java variables for GUI
+    String redVal;
+    String greenVal;
+    String blueVal;
+    String hexVal;
     TextView tv_j_am_redNumVal;
-    SeekBar sb_j_am_redSlider;
-
     TextView tv_j_am_greenNumVal;
-    SeekBar sb_j_am_greenSlider;
-
     TextView tv_j_am_blueNumVal;
+    SeekBar sb_j_am_redSlider;
+    SeekBar sb_j_am_greenSlider;
     SeekBar sb_j_am_blueSlider;
-
     TextView tv_j_am_hexVal;
-
-    Button btn_j_am_save;
-
-    ListView lv_j_am_listOfColors;
-
-    //ArrayList class here
-    ArrayList<RGB> listOfColors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -41,55 +37,122 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         //Connect the Java variables to corresponding GUI variables [HERE]
+        //red
         tv_j_am_redNumVal = findViewById(R.id.tv_v_am_redNumVal);
         sb_j_am_redSlider = findViewById(R.id.sb_v_am_redSlider);
-
+        //green
         tv_j_am_greenNumVal = findViewById(R.id.tv_v_am_greenNumVal);
         sb_j_am_greenSlider = findViewById(R.id.sb_v_am_greenSlider);
-
+        //blue
         tv_j_am_blueNumVal = findViewById(R.id.tv_v_am_blueNumVal);
         sb_j_am_blueSlider = findViewById(R.id.sb_v_am_blueSlider);
-
+        //hex
         tv_j_am_hexVal = findViewById(R.id.tv_v_am_hexVal);
 
-        btn_j_am_save = findViewById(R.id.btn_v_am_save);
-
-        lv_j_am_listOfColors = findViewById(R.id.lv_v_am_listOfColors);
-
-        //Event listener function for red slider
-
-        //Event listener function for green slider
-
-        //Event listener function for blue slider
-
-        //Function for save button
-        saveButtonClickEvent();
-
-        //new ArrayList
-        listOfColors = new ArrayList<RGB>();
-
-        //Function to fill list
+        //Call event listeners for seek bars
+        numValEventListener();
 
     }
 
-    //Function for button click here
-    private void saveButtonClickEvent()
+    //Event listener functions for seek bars
+    private void numValEventListener()
     {
-        btn_j_am_save.setOnClickListener(new View.OnClickListener()
+        //Listener for red
+        sb_j_am_redSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
         {
             @Override
-            public void onClick(View view)
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b)
             {
-                //Function for adding the color to the list
-                addColorToList();
+                //Pull the value of red from the current slider listener
+                tv_j_am_redNumVal.setText(String.valueOf(i));
+                //Convert the int value of red to hex
+                redVal = Integer.toHexString(i);
+                //Convert the int value of green to hex
+                greenVal = Integer.toHexString(sb_j_am_greenSlider.getProgress());
+                //Convert the int value of blue to hex
+                blueVal = Integer.toHexString(sb_j_am_blueSlider.getProgress());
+                //Concatenate the current hex values of rgb into a single string value
+                hexVal = "#" + redVal + greenVal + blueVal;
+                //Set the text of the text view for hexVal & capitalize
+                tv_j_am_hexVal.setText(hexVal.toUpperCase());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar)
+            {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar)
+            {
+
+            }
+        });
+
+        //Listener for green
+        sb_j_am_greenSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+        {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b)
+            {
+                //Pull the value of green from the current slider listener
+                tv_j_am_greenNumVal.setText(String.valueOf(i));
+                //Convert the int value of red to hex
+                redVal = Integer.toHexString(sb_j_am_redSlider.getProgress());
+                //Convert the int value of green to hex
+                greenVal = Integer.toHexString(i);
+                //Convert the int value of blue to hex
+                blueVal = Integer.toHexString(sb_j_am_blueSlider.getProgress());
+                //Concatenate the current hex values of rgb into a single string value
+                hexVal = "#" + redVal + greenVal + blueVal;
+                //Set the text of the text view for hexVal & capitalize
+                tv_j_am_hexVal.setText(hexVal.toUpperCase());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar)
+            {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar)
+            {
+
+            }
+        });
+
+        //Listener for blue
+        sb_j_am_blueSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+        {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b)
+            {
+                tv_j_am_blueNumVal.setText(String.valueOf(i));
+                //Convert the int value of red to hex
+                redVal = Integer.toHexString(sb_j_am_redSlider.getProgress());
+                //Convert the int value of green to hex
+                greenVal = Integer.toHexString(sb_j_am_greenSlider.getProgress());
+                //Convert the int value of blue to hex
+                blueVal = Integer.toHexString(i);
+                //Concatenate the current hex values of rgb into a single string value
+                hexVal = "#" + redVal + greenVal + blueVal;
+                //Set the text of the text view for hexVal & capitalize
+                tv_j_am_hexVal.setText(hexVal.toUpperCase());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar)
+            {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar)
+            {
+
             }
         });
     }
-
-    //Function for saving the color to the lv
-    private void addColorToList()
-    {
-        //
-    }
-
 }
